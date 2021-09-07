46d085a61af2854765bf0dac2b0787e76ffffb5a
NordiCelt <tribouletr@gmail.com>
2021-09-07 06:06:52 -0500

    Add project files.

---
+++ ControlFlowExercise1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ControlFlowExercise1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What's good homie? How many years have you studied C#?");
+            short experience = Convert.ToInt16(Console.ReadLine());
+
+            if (experience >= 1 && experience <= 10)
+                {
+                    Console.WriteLine($"You've been learning C# for {experience} years!");
+                }
+            else
+            {
+                Console.WriteLine("This cant be true!");
+            }
+        }
+    }
+}
